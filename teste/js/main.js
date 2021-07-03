8875f94a70c22412f91a1ebb844556bcbf21acc4
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-07-02 22:01:34 -0300

    Outro modelo de anagrama, foi adicionado conteúdo no JS.

---
+++ teste/js/main.js
@@ -0,0 +1,19 @@
+var arr = ['daiane'];
+
+var allAnagrams = function(arr) {
+    var anagrams = {};
+    arr.forEach(function(str) {
+        var recurse = function(ana, str) {
+            if (str === '') 
+                anagrams[ana] = 1;
+            for (var i = 0; i < str.length; i++)
+                recurse(ana + str[i], str.slice(0, i) + str.slice(i + 1));
+        };
+        recurse('', str);
+    });
+    return Object.keys(anagrams);
+}
+
+console.log(allAnagrams(arr));
+
+
